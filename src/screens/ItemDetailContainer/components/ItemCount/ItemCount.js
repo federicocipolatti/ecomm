8100662c66eaeca134e748db09fb53ff67d996c5
Federico Cipolatti <federicocipolatti@gmail.com>
2021-07-10 17:00:10 -0300

    item count

--- src/screens/ItemDetailContainer/components/ItemCount/ItemCount.js
+++ src/screens/ItemDetailContainer/components/ItemCount/ItemCount.js
@@ -1,22 +1,4 @@
 import React, { useState } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography'
-import { PinDropRounded } from '@material-ui/icons';
-
-const useStyles = makeStyles({
-  root: {
-    maxWidth: 345,
-  },
-  media: {
-    height: 140,
-  },
-});
 
 export const ItemCount = props => {
 
@@ -35,31 +17,12 @@ export const ItemCount = props => {
   }
 
   return (
-    <Card className={classes.root}>
-      <CardActionArea>
-        <CardMedia
-          className={classes.media}
-          image={props.img}
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="h2">
-            {props.titulo}
-          </Typography>
-          <Typography variant="body2" color="textPrimary" component="p">
-            {
-              num
-            }
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-      <CardActions>
-        <Button onClick={handleDecrement} size="large" color="secondary">
-          MENOS
-        </Button>
-        <Button onClick={handleIncrement} size="large" color="primary">
-          MÁS
-        </Button>
-      </CardActions>
-    </Card>
-  );
+    <div className='count'>
+      {num}
+      <div className='buttons'>
+        <button className='btn btn-light btn-outline-dark rounded-0' onClick={handleDecrement}></button>
+        <button className='btn btn-light btn-outline-dark rounded-0' onClick={handleIncrement}></button>
+      </div>
+    </div>
+  )
 }
